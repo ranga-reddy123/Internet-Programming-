0668e26e18ddc119f43b0b4264ff71679b730ea4
ranga-reddy123 <159864985+ranga-reddy123@users.noreply.github.com>
2026-08-06 11:06:07 +0530

    Add files via upload

---
+++ Lab experiments/style.css
@@ -0,0 +1,16 @@
+function login()
+{
+    var user=document.getElementById("user").value;
+
+    var pass=document.getElementById("pass").value;
+
+    if(user=="admin" && pass=="1234")
+        alert("Login Successful");
+    else
+        alert("Invalid Login");
+}
+
+function bookRoom()
+{
+    alert("Room Booked Successfully!");
+}
